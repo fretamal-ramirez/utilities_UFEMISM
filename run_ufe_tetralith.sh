7777eb0a8653bca3e170a456137383108f9a082d
fretamal <franco.retamal@natgeo.su.se>
2025-08-14 13:09:15 +0200

    run one bash file

--- run_ufe_tetralith.sh
+++ run_ufe_tetralith.sh
@@ -6,4 +6,12 @@
 #SBATCH -o out_run_UFEMISM
 #SBATCH -e err_run_UFEMISM
 
-mpiexec UFEMISM_program /home/x_frare/utilities_UFEMISM/config-files_tetralith/config_tetralith_PD_spinup.cfg
+mpiexec UFEMISM_program /home/x_frare/utilities_UFEMISM/config-files_tetralith/config_tetralith_PMIP_AWIESM1.cfg
+
+mpiexec UFEMISM_program /home/x_frare/utilities_UFEMISM/config-files_tetralith/config_tetralith_PMIP_INM-CM4-8.cfg
+
+mpiexec UFEMISM_program /home/x_frare/utilities_UFEMISM/config-files_tetralith/config_tetralith_PMIP_MIROC-ES2L.cfg
+
+mpiexec UFEMISM_program /home/x_frare/utilities_UFEMISM/config-files_tetralith/config_tetralith_PMIP_MPI-ESM1-2.cfg
+
+
